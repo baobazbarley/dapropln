10aba360951d08ed97979c073928049d817b50f6
mizan <baobaz@tlab.co.id>
2016-04-19 06:04:51 +0700

    first commit

---
+++ bonfire/application/views/admin/content/index.php
@@ -0,0 +1,3 @@
+<h1>Dashboard</h1>
+
+<p>This is dashboard brow...</p>
---
+++ bonfire/application/views/home/index.php
@@ -0,0 +1,12 @@
+<div class="jumbotron" text-align="center">
+	<h1>Welcome to DAPRO PLN</h1>
+	<br/>
+	<br/>
+
+	<?php if (isset($current_user->email)) : ?>
+		<a href="<?php echo site_url(SITE_AREA) ?>" class="btn btn-large btn-success">Go to the Admin area</a>
+	<?php else :?>
+		<a href="<?php echo site_url(LOGIN_URL); ?>" class="btn btn-large btn-primary"><?php echo lang('bf_action_login'); ?></a>
+	<?php endif;?>
+
+</div>
